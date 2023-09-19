4283f773aa9e35e60feb19332b98845f9f399268
Williams Abraham <wabraham@vatebra.com>
2023-09-19 22:22:12 +0100

    logic improvement

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read");
     
-            let guess: u32 = guess.trim().parse().expect("Please input a valid number");
+            let guess: u32 = match guess.trim().parse()
+            {
+                Ok(num) => num,
+                Err(_) => 
+                {
+                    println!("Your input was invalid");
+                    continue;
+                }
+            };
 
         match guess.cmp(&secret)    
         {
@@ -22,7 +30,7 @@ fn main() {
                 println!("You won");
                 break;
             }
-        }
+        };
 
         println!("You guessed: {guess}");
         println!("The answer is: {} =====> You guessed :{1}",secret,guess);
